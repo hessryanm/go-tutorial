5507549833dc558b32eb9215c6516000064b2ec5
Ryan Hess <hess@Ryans-Air.localdomain>
2024-03-14 13:54:35 -0600

    Working GET and POST endpoints

--- server.go
+++ server.go
@@ -1,34 +1,89 @@
 package main
 
 import(
+	"database/sql"
 	"log"
+
+	_ "github.com/lib/pq"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func indexHandler(c *fiber.Ctx) error {
+type todo struct {
+	Item string
+}
+
+func handleError(c *fiber.Ctx, errStr string) error {
+	errStr = "An error occurred: " + errStr
+	
+	log.Fatalln(errStr)
+	return c.SendString(errStr)
+}
+
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res todo
+	var todos []todo
+
+	rows, err := db.Query("SELECT * FROM todos")
+	defer rows.Close()
+
+	if err != nil {
+		return handleError(c, err.Error())
+	}
+
+	for rows.Next() {
+		rows.Scan(&res.Item)
+		todos = append(todos, res)
+	}
+
+	return c.JSON(todos)
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	newTodo := todo{}
+	if err := c.BodyParser(&newTodo); err != nil {
+		return handleError(c, err.Error())
+	}
+
+	if newTodo.Item != "" {
+		_, err := db.Exec("INSERT INTO todos VALUES ($1)", newTodo.Item)
+		if err != nil {
+			return handleError(c, err.Error())
+		}
+	}
+
+	return c.Redirect("/")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Not Implemented")
 }
 
-func postHandler(c *fiber.Ctx) error {
-	return c.SendString("Not Implemented")
-}
-
-func putHandler(c *fiber.Ctx) error {
-	return c.SendString("Not Implemented")
-}
-
-func deleteHandler(c *fiber.Ctx) error {
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Not Implemented")
 }
 
 func main() {
+	dbConnStr := "postgresql://hess@localhost/postgres?sslmode=disable"
+	db, err := sql.Open("postgres", dbConnStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	app := fiber.New()
 
-	app.Get("/", indexHandler)
-	app.Post("/", postHandler)
-	app.Put("/update", putHandler)
-	app.Delete("/delete", deleteHandler)
+	app.Get("/", func(c *fiber.Ctx) error {
+        return indexHandler(c, db)
+    })
+	app.Post("/", func(c *fiber.Ctx) error {
+        return postHandler(c, db)
+    })
+	app.Put("/update", func(c *fiber.Ctx) error {
+        return putHandler(c, db)
+    })
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+        return deleteHandler(c, db)
+    })
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatalln(app.Listen(":3000"))
 }
